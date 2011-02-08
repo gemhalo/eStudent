59e3362e0f7aefdb98cd9212dc969579c8b77ff6
Haftekiros <hafte.23@gmail.com>
2011-02-08 17:21:18 +0300

    Login process latest

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,28 @@ class User < ActiveRecord::Base
 	def password_must_be_present
 		errors.add(:password, "Missing ") unless hashed_password.present?
 	end
+class << self
+    def authenticate(name, password)
+         logger.info("--from auth---username(param)---------------------#{name}---------------------------")
+         logger.info("---from auth--password(param)---------------------#{password}---------------------------")
+        if user = find_by_user_name(name)
+           if user.hashed_password == encrypt_password(password, user.salt)
+              user
+           end
+        end
+    end
+    def encrypt_password(password, salt)
+      Digest::SHA2.hexdigest(password + "wibble" + salt)
+    end
+  end
+
+  def password=(password)
+    @password = password
+    if password.present?
+      generate_salt
+      self.hashed_password = self.class.encrypt_password(password, salt)
+    end
+  end
 
 	def encrypt_password(password, salt)
 		Digest::SHA2.hexdigest(password + "wibble" + salt)
