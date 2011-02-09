c27a6032ecfd98c950534468e79f243ce4c088d9
Haftsh <railsguru1@gmail.com>
2011-02-09 09:36:29 +0300

    rSpec Test Added.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+#	before do
+#		@user=User.new
+#	end
+	
+
+  it "username must not be empty" do
+		user = User.new
+		user.user_name = "Haftsh"
+		user.user_name.should_not be_blank
+	end
+
+  it "password must not be empty" do
+		user = User.new
+		user.password = " "
+		user.password.should_not be_blank
+	end
+
+#  it "must have a user name" do
+#    @user.user_name = 'abebe'
+#    @user.should have(0).error_on(:user_name)
+#  end
+
+
 end
