5dc0553f22255989e351df2bd49b043825cb8809
G/yohannes Zenebe <gyohannes1@gmail.com>
2011-03-03 12:07:13 +0300

    modifications made locally

--- app/controllers/dormitory_placement_controller.rb
+++ app/controllers/dormitory_placement_controller.rb
@@ -8,6 +8,8 @@ class DormitoryPlacementController < ApplicationController
     @building=Building.find(params[:building_name])
     @collegesCollege.find(params[:college])
     @students=Student.all(:conditions=>["college=@college and sex=params[:sex]"])
+    @rooms=@building.number_of_rooms
+    @beds=@building.number_of_beds_per_room
     @totalbeds=@building.number_of_rooms * @building.number_of_beds_per_room
     
   end
