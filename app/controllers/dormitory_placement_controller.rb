41e9a58e1e428656d2d9b180308eda1896516773
G/yohannes Zenebe <gyohannes1@gmail.com>
2011-03-03 16:52:59 +0300

    dorm placement

--- app/controllers/dormitory_placement_controller.rb
+++ app/controllers/dormitory_placement_controller.rb
@@ -11,6 +11,15 @@ class DormitoryPlacementController < ApplicationController
     @rooms=@building.number_of_rooms
     @beds=@building.number_of_beds_per_room
     @totalbeds=@building.number_of_rooms * @building.number_of_beds_per_room
-    
+
+    for s in @students
+      for r in 1..@rooms
+        for b in 1..@beds
+          s.dorm="#{@building} " << "Room " << "#{r}" << "Bed No " << "#{b}"
+        end
+      end
+    end
   end
+
+  
 end
