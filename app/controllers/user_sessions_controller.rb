bfb399dbbef8d18da0a6a62b72228569c600fd21
G/yohannes Zenebe <gyohannes1@gmail.com>
2011-03-04 10:55:58 +0300

    modification

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -34,7 +34,7 @@ class UserSessionsController < ApplicationController
     @user_session.destroy
 
     respond_to do |format|
-      format.html { redirect_to(:users, :notice => 'Goodbye!') }
+      format.html { redirect_to(:action=>'new', :notice => 'Goodbye!') }
       format.xml { head :ok }
     end
   end
