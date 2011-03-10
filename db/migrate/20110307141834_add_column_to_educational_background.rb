78d9171f8a60736da391272da66a4a6fc0cea44c
Dereje Haddis Engida <dereje@jhaddis.(none)>
2011-03-10 22:06:52 +0300

    Merged files

--- db/migrate/20110307141834_add_column_to_educational_background.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnToEducationalBackground < ActiveRecord::Migration
-  def self.up
-    add_column :educational_backgrounds, :student_id, :integer
-  end
-
-  def self.down
-    remove_column :educational_backgrounds, :student_id
-  end
-end
