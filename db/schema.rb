e65e42b58aea5f2405580e31f7a205ec6d5fbb05
zigniter <samsonya@gmail.com>
2011-03-11 12:56:28 +0300

    local commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110310203006) do
+ActiveRecord::Schema.define(:version => 20110311093046) do
 
   create_table "abilities", :force => true do |t|
     t.datetime "created_at"
@@ -71,9 +71,6 @@ ActiveRecord::Schema.define(:version => 20110310203006) do
     t.integer  "admission_status"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "verified",                 :default => false
-    t.integer  "student_service_staff_id"
-    t.integer  "admission_type_id"
   end
 
   create_table "award_types", :force => true do |t|
@@ -90,15 +87,6 @@ ActiveRecord::Schema.define(:version => 20110310203006) do
     t.datetime "updated_at"
   end
 
-  create_table "campus", :force => true do |t|
-    t.string   "name"
-    t.integer  "campus_telephone"
-    t.string   "campus_pobox"
-    t.string   "campus_email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "campuses", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -108,7 +96,10 @@ ActiveRecord::Schema.define(:version => 20110310203006) do
   create_table "colleges", :force => true do |t|
     t.string   "name"
     t.integer  "campus_id"
-    t.string   "dean"
+    t.integer  "college_telephone"
+    t.string   "college_pobox"
+    t.string   "college_email"
+    t.integer  "instructor_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -199,7 +190,6 @@ ActiveRecord::Schema.define(:version => 20110310203006) do
     t.integer  "attachment_file_size"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "student_id"
   end
 
   create_table "emergency_contacts", :force => true do |t|
@@ -356,17 +346,6 @@ ActiveRecord::Schema.define(:version => 20110310203006) do
     t.boolean  "disability"
     t.string   "type_of_disability"
     t.integer  "region_code"
-  end
-
-  create_table "programs", :force => true do |t|
-    t.string   "name"
-    t.integer  "department_id"
-    t.integer  "admission_id"
-    t.date     "duration"
-    t.integer  "total_credit_hour"
-    t.integer  "award_type_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "references", :force => true do |t|
