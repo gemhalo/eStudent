ade813ae640a1aa113d0d5d9bcba1d8ceecc2d87
haftekiros redae <hafte.23@gmail.com>
2011-04-05 12:25:42 +0300

    Curriculum table created again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110401131638) do
+ActiveRecord::Schema.define(:version => 20110405091724) do
 
   create_table "abilities", :force => true do |t|
     t.datetime "created_at"
@@ -201,14 +201,11 @@ ActiveRecord::Schema.define(:version => 20110401131638) do
   end
 
   create_table "curriculums", :force => true do |t|
-    t.integer  "course_id"
     t.integer  "program_id"
-    t.integer  "course_type"
-    t.integer  "semester_id"
     t.integer  "class_year_id"
+    t.integer  "semester_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.float    "price"
   end
 
   create_table "department_choices", :force => true do |t|
@@ -306,13 +303,12 @@ ActiveRecord::Schema.define(:version => 20110401131638) do
   end
 
   create_table "events", :force => true do |t|
-    t.integer  "academic_calendar_id"
-    t.date     "start_date"
-    t.date     "end_date"
-    t.string   "title"
-    t.string   "detail"
+    t.string   "name"
+    t.datetime "start_at"
+    t.datetime "end_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "details"
   end
 
   create_table "family_backgrounds", :force => true do |t|
