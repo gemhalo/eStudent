8fc34499d73f2c4b41f45f8e5e5e960ae3d1fa56
zigniter <samsonya@gmail.com>
2011-04-07 15:16:00 +0300

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -338,6 +338,15 @@ ActiveRecord::Schema.define(:version => 20110405114619) do
     t.datetime "updated_at"
   end
 
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "details"
+  end
+
   create_table "family_backgrounds", :force => true do |t|
     t.string   "father_edu_level"
     t.string   "mother_edu_level"
